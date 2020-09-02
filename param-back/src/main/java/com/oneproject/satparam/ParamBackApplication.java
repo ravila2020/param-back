6f71fb07f58dbff5560ee016df1d4795c774b8c1
roberto.avila <roberto.avila@UANLMEX-A077.uanlmexico.mx>
2020-09-01 21:26:32 -0500

    Base del backend de SAT-PARAM

---
+++ param-back/src/main/java/com/oneproject/satparam/ParamBackApplication.java
@@ -0,0 +1,13 @@
+package com.oneproject.satparam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParamBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParamBackApplication.class, args);
+	}
+
+}
---
+++ param-back/src/test/java/com/oneproject/satparam/ParamBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oneproject.satparam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParamBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
