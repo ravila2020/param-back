8e60124f70e1c7296190db786ce7ea62f79536a3
jose.mancera <jose.mancera@UANLMEX-A097.uanlmexico.mx>
2020-09-09 14:14:49 -0500

    Otro PBA JAMD

---
+++ param-back/src/main/java/com/oneproject/satparam/modelo/Mpdt025Id.java
@@ -0,0 +1,50 @@
+package com.mtto.sat25.modelo;
+
+import java.io.Serializable;
+
+public class Mpdt025Id implements Serializable {
+
+	private String  codent;
+	
+
+	public Mpdt025Id() {
+		this.codent  = codent;
+	}
+	
+	public Mpdt025Id (String codent){
+		this.codent = codent;
+	}
+
+		
+	public String getCodent() {
+		return codent;
+	}
+	
+		public void setCodent(String codent) {
+		this.codent = codent;
+	}
+
+	
+}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+
+
+
+
