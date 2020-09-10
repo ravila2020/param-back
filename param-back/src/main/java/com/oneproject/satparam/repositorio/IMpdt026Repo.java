a87c52929c9637bd46286771ceb535f738b9ea53
usuario <usuario@DESKTOP-C75Q50A>
2020-09-10 14:39:15 -0500

    implemantado por alex piña

---
+++ param-back/src/main/java/com/oneproject/satparam/repositorio/IMpdt026Repo.java
@@ -0,0 +1,10 @@
+package com.oneproject.satparam.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.oneproject.satparam.modelo.Mpdt026;
+import com.oneproject.satparam.modelo.Mpdt026Id;
+
+public interface IMpdt026Repo extends JpaRepository<Mpdt026 ,Mpdt026Id>{
+
+}
