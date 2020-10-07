b105db3ee57a5837d2f70fab3aa3137bcd5f3ff3
Admin <ahermed@outlook.com>
2020-10-07 12:32:34 -0500

    Retorno de versión correcta

--- param-back/src/main/java/com/oneproject/satparam/modelo/Mpdt026.java
+++ param-back/src/main/java/com/oneproject/satparam/modelo/Mpdt026.java
@@ -52,13 +52,6 @@ public class Mpdt026 {
 		return codent;
 	}
 	
-	public String getCodent52() {
-		return codent;
-	}
-	public String getCodent51() {
-		return codent ;
-	}
-
 	public void setCodent(String codent) {
 		this.codent = codent;
 	}
