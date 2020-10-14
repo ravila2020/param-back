f2b77585cf5863ff8457a0aa1573867f7fb0794b
AlbertoOneproject <albertosanchez@oneproject.com>
2020-10-13 22:56:20 -0500

    Add Mpdt061

--- param-back/src/main/java/com/oneproject/satparam/repositorio/IMpdt061Repo.java
+++ param-back/src/main/java/com/oneproject/satparam/repositorio/IMpdt061Repo.java
@@ -9,7 +9,6 @@ import com.oneproject.satparam.modelo.Mpdt061;
 
 public interface IMpdt061Repo extends JpaRepository<Mpdt061, String>{
 
-
 	
 	@Query("select DISTINCT  (m.Binpre) from Mpdt061 m")
 	List<String> findByBinpre();
@@ -19,6 +18,5 @@ public interface IMpdt061Repo extends JpaRepository<Mpdt061, String>{
 	
 	@Query("select DISTINCT  (m.Rangoinf) from Mpdt061 m")
 	List<String> findByRangoinf();
-	
 
 }
