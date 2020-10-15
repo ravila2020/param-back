02bdef758e5fb5ddc689f6a5f05cb0bf3cd3d845
Ravila2020 <ravila@oneproject.com>
2020-10-15 18:40:20 -0500

    ajuste  de servicios de mpdt061

--- param-back/src/main/java/com/oneproject/satparam/repositorio/IMpdt061Repo.java
+++ param-back/src/main/java/com/oneproject/satparam/repositorio/IMpdt061Repo.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import com.oneproject.satparam.modelo.Mpdt026;
 import com.oneproject.satparam.modelo.Mpdt061;
 
 public interface IMpdt061Repo extends JpaRepository<Mpdt061, String>{
@@ -13,10 +15,18 @@ public interface IMpdt061Repo extends JpaRepository<Mpdt061, String>{
 	@Query("select DISTINCT  (m.Binpre) from Mpdt061 m")
 	List<String> findByBinpre();
 	
-	@Query("select DISTINCT  (m.Rangosup) from Mpdt061 m")
-	List<String> findByRangosup();
+//	@Query("select DISTINCT  (m.Rangosup) from Mpdt061 m")
+//	List<String> findByRangosup();
 	
-	@Query("select DISTINCT  (m.Rangoinf) from Mpdt061 m")
-	List<String> findByRangoinf();
+//	@Query("select DISTINCT  (m.Rangoinf) from Mpdt061 m")
+//	List<String> findByRangoinf();
+
+	
+	@Query("select DISTINCT  (m.Rangosup) from Mpdt061 m where m.Binpre =:binpre")
+	List<String> findByRangosup(@Param("binpre") String binpre);
+	
+	@Query("select DISTINCT  (m.Rangoinf) from Mpdt061 m where m.Binpre =:binpre and m.Rangosup =:rangosup")
+	List<String> findByRangoinf(@Param("binpre") String binpre, @Param("rangosup") String rangosup);
+	
 
 }
--- param-back/src/main/java/com/oneproject/satparam/rest/RestControllerMpdt061.java
+++ param-back/src/main/java/com/oneproject/satparam/rest/RestControllerMpdt061.java
@@ -6,9 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.oneproject.satparam.modelo.Mpdt026;
 import com.oneproject.satparam.modelo.Mpdt061;
 import com.oneproject.satparam.repositorio.IMpdt061Repo;
 
@@ -33,12 +33,12 @@ public class RestControllerMpdt061 {
 	}
 
 	@GetMapping(path= {"/rangsup"})
-	public List<String> list061Rangsup(){
-		return  repo.findByRangosup();
+	public List<String> list061Rangsup(@RequestParam("binpre") String binpre){
+		return  repo.findByRangosup(binpre);
 	}
 
 	@GetMapping(path= {"/ranginf"})
-	public List<String> list061Ranginf(){
-		return  repo.findByRangoinf();
+	public List<String> list061Ranginf(@RequestParam("binpre") String binpre, @RequestParam("rangsup") String rangsup){
+		return  repo.findByRangoinf(binpre,rangsup);
 	}
 }
